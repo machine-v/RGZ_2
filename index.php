75262868faea09329274950d50730d7fe12526c5
machine-v <volkovma21@mail.ru>
2017-12-10 16:58:36 +0700

    1

--- index.php
+++ index.php
@@ -9,32 +9,35 @@
 			} else {
 				$val1 = '';
 			}
+			
 			if (isset($_GET['val2'])) {
 				$val2 = $_GET['val2'];
 			} else {
 				$val2 = '';
 			}
-			
 		?>
+		
 		<form method="GET" action="index.php">
 			<p>Полярный радиус:<p>
 			<input type="text" name="val1" value="<?= htmlspecialchars($val1) ?>">
-			<p>Полярный угол:<p>
+			<p>Полярный угол (rad):<p>
 			<input type="text" name="val2" value="<?= htmlspecialchars($val2) ?>">
 			<input type="submit" value="Перевести">
 		</form>
 		
 		<?php
+			$val1 = str_replace(',', '.', $val1);
+			$val2 = str_replace(',', '.', $val2);
+			
 			if (is_numeric($val1) && is_numeric($val2)) {
 				$x = $val1 * cos($val2);
 				$y = $val1 * sin($val2);
-				$oX = number_format($x);
-				$oY = number_format($y);
+				$oX = number_format($x, 2, ',', '');
+				$oY = number_format($y, 2, ',', '');
 				echo "Декартовы координаты <br/> x : $oX <br/> y : $oY";
-				}elseif($val1 != '' || $val2 != ''){
+			} else if ($val1 != '' || $val2 != '') {
 				echo 'В значении не могут присутствовать не числовые значения';
-				}
+			}
 		?>
-		
 	</body>
 </html>
